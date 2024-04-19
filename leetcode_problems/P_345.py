aaeb842f778b3d10b50d02f3162e70603bb42278
Yousef <yuossefbakier@gmail.com>
2024-04-20 01:46:48 +0200

    Add Problem P_345 IN leetcode_problems

---
+++ leetcode_problems/P_345.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        s = list(s)
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            if s[l] not in vowels:
+                l += 1
+            if s[r] not in vowels:
+                r -= 1
+            if s[l] in vowels and s[r] in vowels and l < r:
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+        return ''.join(s)
+
+            
+s = Solution()
+print(s.reverseVowels("a."))
