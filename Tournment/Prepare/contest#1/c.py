968899fc23d82a84d92cf9ee80e51986de6e3673
Yousef <yuossefbakier@gmail.com>
2024-05-27 21:06:55 +0300

    Add Problem c IN contest#1

---
+++ Tournment/Prepare/contest#1/c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    t = 1
+    # t = int(input())
+
+    for _ in range(t):
+        n = int(input())
+        x1 = x2 = x3 = n / 3
+        print(x1*x2*x3)
+
