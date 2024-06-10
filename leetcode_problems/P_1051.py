2aab91307588a3583bc3c67f0fca9331380f52ff
Yousef <yuossefbakier@gmail.com>
2024-06-10 22:51:03 +0300

    Add Problem P_1051 IN leetcode_problems

---
+++ leetcode_problems/P_1051.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from typing import List
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        cnt = 0
+        for i in range(len(heights)):
+            if expected[i] != heights[i]:
+                cnt += 1
+        return cnt
