62505d4822117414cdedfa1477ff55fb161a1f85
Yousef <yuossefbakier@gmail.com>
2024-07-27 22:29:07 +0300

    Add Problem 617A IN Problems

---
+++ codeforces/Problems/617A.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from math import ceil
+if __name__ == '__main__':
+    t = 1
+    # t = int(input())
+
+    for _ in range(t):
+        n = int(input())
+        print(ceil(n/5))
